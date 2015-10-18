0d17fc8136bb02af1eb20603e087e5be88b6ccfd
Øyvind Midtbø <oyvind.midtbo@gmail.com>
2015-10-18 11:51:02 +0200

    First commit

---
+++ RuterAppleWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuterAppleWatch
+//
+//  Created by Øyvind Midtbø on 17.10.2015.
+//  Copyright © 2015 Øyvind Midtbø. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
